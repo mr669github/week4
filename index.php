caf75d75c182a895db997924ce276499e78cd350
monisha ramesh <mr669@afsconnect2.njit.edu>
2017-10-03 19:39:21 -0400

    Compared  and

--- index.php
+++ index.php
@@ -23,10 +23,16 @@ class main
    $date = date('Y-m-d', time());
    $tar = "2017/05/24";
    $year = array("2012", "396", "300", "2000", "1100", "1089");
-
+   $chdate = str_replace('-','/',$date);
+   //1
    $this->html .= htmlTags::heading('Replace - with / in Date');
    $this->html .= stringFunctions::replace('-' ,'/' ,$date);
    $this->html .= htmlTags::hrline();
+   //2
+   $this->html .= htmlTags::heading("Compare $chdate with $tar");
+   $this->html .= stringFunctions::compare($chdate,$tar);
+   $this->html .= stringFunctions::printthisarr($input,true);
+   $this->html .= htmltags::hrline();
    }
 
 
@@ -54,14 +60,26 @@ class stringFunctions
   {
   return print_r($input);
   }
-
-
-
+  static public function printthisarr($input1, $input2)
+  {
+  return print_r($input1, $input2);
+  }
+//1
   static public function replace($input1, $input2, $input3)
   { 
    return str_replace($input1,$input2,$input3);
   }
-
+//2
+  static public function compare($input1, $input2)
+  {
+  $res = strcmp($input1,$input2);
+  	if($res > 0)
+	return "The future<br>";
+	else if($res < 0)
+	return "The Past<br>";
+	else 
+	return "Oops";
+  }
 
 }
 
